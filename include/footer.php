639a2ad8b7cd98ef6f079dcb74ade674c48f3967
manalhatem <83021342+manalhatem@users.noreply.github.com>
2022-02-21 19:18:54 +0200

    Add files via upload

---
+++ include/footer.php
@@ -0,0 +1,7 @@
+ <!-- Bootstarp.js-->
+     <script src="js/jquery5.min.js" type="text/javascript"></script>
+     <script src="https://kit.fontawesome.com/03757ac844.js" type="text/javascript"></script>
+    <script src="js/bootstrap.min.js" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ include/header.php
@@ -0,0 +1,17 @@
+<?php require('connection.php'); ?>
+ <!doctype html>
+<html  lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap-->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <!--link rel="stylesheet" type="text/css" href="css/bootstrap-rtl.css"-->
+    <link rel="stylesheet" type="text/css" href="css/Dashboard.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+</head>
+<body>
